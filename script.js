7503e63e6288721baaf347d7f851125c1f928319
FerZingaretti <111395725+FerZingaretti@users.noreply.github.com>
2022-11-08 20:16:31 -0300

    cambiando a Objetos

--- script.js
+++ script.js
@@ -19,82 +19,40 @@ do{
 
 
 
-let materia = Number (prompt ("en que materia desea registrar? 1-MAtematica 2-ciencias"))
 
-switch (materia){
+class Alumno{
+    constructor (nombre, apellido, nota1, nota2, nota3,promedio ){
+        this.nombre = nombre.toUpperCase();
+        this.apellido = apellido.toUpperCase();
+        this.nota1 = nota1;
+        this.nota2 = nota2; 
+        this.nota3 = nota3;
+        this.promedio = ((nota1+nota2+nota3)/3)
+    }
+nombrando(){
+    console.log("el alumno ingresado es"+this.nombre +  "" + this.apellido)
+}
 
-    case 1: 
+}
 
-        alert ("Usted ingresara las notas de Matematica")
-         var nota1 =prompt("ingrese su calificacion 1");
-            if ((nota1>=7 )&& (nota1<=10)){
-           alert ("aprobado")
-            } else  if (nota1<7 && nota1<=1){
-           alert ("desaprobado")
-            }/* else alert ("nota invalida") */
-       
-       
-         var nota2 = prompt(" ingrese su calificacion 2");
-             if ((nota2>=7 )&& (nota2<=10)){
-           alert ("aprobado")
-             } else  if ((nota2<7) && (nota2<=1)){
-           alert ("desaprobado")
-            }/* else alert ("nota invalida") */ 
-       
-         var nota3 = prompt("ingrese su calificacion 3");
-             if ((nota3>=7 )&& (nota3<=10)){
-           alert ("aprobado")
-              } else  if ((nota3<7) && (nota3<=1)){
-           alert ("desaprobado")
 
-           
-        break
 
+const alumno1 = new Alumno ("Juan", "Sanchez", " 8", "9", "9",)
+
+alumno1.nombrando
+console.log (alumno1.promedio)
+
+
+
+
+
+/*  promediando (){
+    this.promedio = ((this.nota1 + this.nota2 + this.nota3)/3) */
     
+/* console.log ( "el promedio de matematicas de" + alumno1.nombre + " " + alumno1.apellido + "es" + alumno1.promedio) */
+
 
         
-        
-        }
-    case 2:
-        alert ("Usted ingresara las notas de Ciencias")
-         var nota1 =prompt("ingrese su calificacion 1");
-             if ((nota1>=7 )&& (nota1<=10)){
-           alert ("aprobado")
-             } else  if (nota1<7 && nota1<=1){
-           alert ("desaprobado")
-             }/* else alert ("nota invalida") */
-       
-       
-         var nota2 = prompt(" ingrese su calificacion 2");
-             if ((nota2>=7 )&& (nota2<=10)){
-           alert ("aprobado")
-              } else  if ((nota2<7) && (nota2<=1)){
-           alert ("desaprobado")
-              }/* else alert ("nota invalida") */ 
-       
-           var nota3 = prompt("ingrese su calificacion 3");
-              if ((nota3>=7 )&& (nota3<=10)){
-           alert ("aprobado")
-              } else  if ((nota3<7) && (nota3<=1)){
-           alert ("desaprobado")
-            }
-        break
-        
 
-    default:
-        alert ("usted ingreso incorrectamente las notas")
-
-}
-
-
-
-function prom (nota1,nota2,nota3){
-    Resultado  = ((nota1+nota2+nota3)/3)
-}   
-
-/* let promedio=((nota1+nota2+nota3)/3)
-
-if (promedio>=7){
-    alert ("Felicitaciones estas aprobado")
-}
-else alert ("no fue suficiente, a reforzar lo visto!") */
+  
+ 
